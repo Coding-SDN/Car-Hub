b1c6ec8445462978b402dc29dc523ebbb8e4d8ab
Steeve <steevemx98@gmail.com>
2023-08-07 16:29:49 +0100

    Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 export const metadata = {
   title: 'Car Hub',
   description: 'Discover the best cars in the world.',
+  image: "/logo.svg",
 }
 
 export default function RootLayout({
