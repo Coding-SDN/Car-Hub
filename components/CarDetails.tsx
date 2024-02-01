66e2fd5270322a030bf70b140324b73e404991e5
MatTizx <107066928+Coding-SDN@users.noreply.github.com>
2024-02-01 23:45:41 +0100

    Update CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import { Fragment } from "react";
 import Image from "next/image";
 
 import { Dialog, Transition } from "@headlessui/react";
-import { CarProps } from "@types";
-import { generateCarImageUrl } from "@utils";
+import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDetailsProps {
   isOpen: boolean;
