64cbc28fada1ea433a7d0140410a2e5eda353a6c
MatTizx <107066928+Coding-SDN@users.noreply.github.com>
2024-02-01 23:35:46 +0100

    Update CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import Image from "next/image";
 
-import { Dialog, Transition } from "@headlessui";
+import { Dialog, Transition } from "@headlessui/react";
 import { CarProps } from "@types";
 import { generateCarImageUrl } from "@utils";
 
