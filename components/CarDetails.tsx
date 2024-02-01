73fe5b46668d69bd597d17eb9ffc8ce82b196dfb
MatTizx <107066928+Coding-SDN@users.noreply.github.com>
2024-02-01 23:16:06 +0100

    Update CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import { Fragment } from "react";
 import Image from "next/image";
 
 import { Dialog, Transition } from "@headlessui/react";
-import { CarProps } from "@/types";
-import { generateCarImageUrl } from "@/utils";
+import { CarProps } from "@types";
+import { generateCarImageUrl } from "@utils";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -65,7 +63,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                       <Image src={generateCarImageUrl(car, "29")} alt='car model' fill priority className='object-contain' />
                     </div>
                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                      <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='mt-5 w-[20px] object-contain' />
+                      <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='object-contain' />
                     </div>
                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                       <Image  src={generateCarImageUrl(car, "13")} alt='car model' fill priority className='object-contain' />
