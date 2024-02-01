6e9e896f61dce42c5dba8b74a2937870614f4a61
MatTizx <107066928+Coding-SDN@users.noreply.github.com>
2024-02-01 23:54:41 +0100

    Update CarDetails.tsx

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Fragment } from "react";
 import Image from "next/image";
 
@@ -60,7 +62,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
 
                   <div className='flex gap-3'>
                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
-                      <Image src={generateCarImageUrl(car, "29")} alt='car model' fill priority className='object-contain' />
+                      <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='mt-5 w-[20px] object-contain' />
                     </div>
                     <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                       <Image src={generateCarImageUrl(car, "33")} alt='car model' fill priority className='object-contain' />
