09df99336f09fccc1b83c4415a460b8f47a03cea
MatTizx <107066928+Coding-SDN@users.noreply.github.com>
2024-02-01 23:06:39 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import './globals.css'
 export const metadata = {
   title: 'CarHub | Find, Book or Rent a Car Online',
   description: 'Discover the best cars in the world.',
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
 }
 
 export default function RootLayout({
