edd655b81006bd8eb4319948b321a740927c8f30
MatTizx <107066928+Coding-SDN@users.noreply.github.com>
2024-02-01 17:59:41 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <header className="w-full absolute z-10">
         <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
             <Link href="/" className="flex items-center justify-center">
-                <Image src="/Logo.svg" alt="Car Hub Logo" width={118} height={18} className="object-contain"/>
+                <Image src="/logo.svg" alt="Car Hub Logo" width={118} height={18} className="object-contain"/>
             </Link>
 
             <CustomButton 
